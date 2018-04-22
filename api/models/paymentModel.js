a8fe568b9ee4f7a4a95e35aad4df85ef3c075c7f
Muhammad <muhammad.s@comsoftltd.com>
2018-04-22 19:36:28 +0100

    new update

--- api/models/paymentModel.js
+++ api/models/paymentModel.js
@@ -2,18 +2,20 @@
 var mongoose = require ('mongoose');
 var Schema = mongoose.Schema
 
-var userPaymentSchema = new Schema ({
+var userPaymentSchema = new Schema ([{
 
     userId:{type:String},
     username:{type:String},
     email:{type:String},
+    quantity:{type:String},
+    paystatus:{type:String},
     phoneNumber:{type:String},
     uniqueRef:{type:String},
     bankCode:{type:String},
     accountNumber:{type:String},
-    amountWithdrawn:{type:String},
-    
-},{
+    amount:{type:String},
+
+}],{
     timestamps: true
   });
   
