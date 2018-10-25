5e187f765d3df8be7c99edd416218d6d53ebf505
Olacoded <apple@Olacoded-MBPro.local>
2018-10-25 17:56:36 +0100

    image post

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -10,7 +10,7 @@ var imageWare = require('../middleware/imagefile')
     router.get('/get/:userId', user_controller.userbyid);  //endpoint for retrieving single user by id GET
     router.put('/update', user_controller.updateuserProfile); //endpoint to update users profile  PUT
     router.delete('/delete/:userId', user_controller.deleteUser); //endpoint to delete user by Id DELETE
-    router.post('/uploadImage', user_controller.saveImage); //endpoint to upload user Image POST
+    router.post('/uploadImage',imageWare, user_controller.saveImage); //endpoint to upload user Image POST
     router.get('/getAuth', user_controller.signedHeader); //endpoint to get  Authorized User based on token
 
 //Medical Information Route
